5c2231fa86f882ecab4494b726f2516ed1532701
1Nelsonel <nelsonmokoro37@gmail.com>
2022-10-10 09:18:10 +0300

    success

---
+++ bank_modern_app/src/App.jsx
@@ -0,0 +1,37 @@
+import styles from './style';
+
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer  } from './components';
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    {/* navbar section */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    {/* hero section */}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+
+    {/* main section */}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing /> 
+        <Testimonials /> 
+        <Clients /> 
+        <CTA /> 
+        <Footer /> 
+      </div>
+    </div>
+
+  </div>
+);
+
+export default App
