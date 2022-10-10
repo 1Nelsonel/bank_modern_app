b69fa1a410735ae09583ac2cb2f3ce83184cfd2e
1Nelsonel <nelsonmokoro37@gmail.com>
2022-10-10 15:00:27 +0300

    Business success

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import { apple, bill, google } from '../assets';
+import styles, { layout } from '../style';
 
-const Billing = () => {
-  return (
+const Billing = () => (
     <div>Billing</div>
-  )
-}
+)
 
 export default Billing
