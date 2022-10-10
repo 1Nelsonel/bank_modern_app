3a7a2c9be4788a411b11da625c62b496aad7b190
1Nelsonel <nelsonmokoro37@gmail.com>
2022-10-10 18:46:49 +0300

    Footer success

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       {/* nav links */}
       <ul className='list-none sm:flex hidden justify-end item-center flex-1'>
         {navLinks.map((nav, index) => (
-          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] 
+          <li key={nav.id} className={`font-poppins font-normal hover:text-secondary cursor-pointer text-[16px] 
                             ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white`}>
             <a href={`#${nav.id}`}>
               {nav.title}
@@ -34,7 +34,7 @@ const Navbar = () => {
 
           <ul className='list-none sm:flex flex-col justify-end item-center flex-1'>
             {navLinks.map((nav, index) => (
-              <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] 
+              <li key={nav.id} className={`font-poppins font-normal hover:text-secondary cursor-pointer text-[16px] 
                             ${index === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white`}>
                 <a href={`#${nav.id}`}>
                   {nav.title}
